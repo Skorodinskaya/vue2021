79b2be059d4c2f656e997d5fda237883688d8add
Skorodinskaya <skorodinskaya01@gmail.com>
2021-12-08 00:26:33 +0200

    lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
+  <button @click="isVisible = !isVisible">toggle list visibility</button>
+  <Test v-if="isVisible" :items="items" @removeItem="removeItemFromList"/>
+  <h1 v-else-if="!isVisible && counter === 0">Some strange condition</h1>
+  <h1 v-else>List is hidden</h1>
+
+
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 v-show="isVisible">{{user.name}}</h1>
+  <div>counter value: {{counter}}</div>
+  <button @click="incCounter($event, 123321)">inc</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isVisible: true,
+      items:[{id: 1, text: 'text1'},{id: 2, text: 'text2'},{id: 3, text: 'text3'},],
+      counter: 0,
+      username: 'Anastasiia',
+      user:{
+        name: 'Grande'
+      }
+    }
+  },
+  methods: {
+    removeItemFromList(id){
+      this.items = this.items.filter(item => item.id !==id)
+    },
+
+    incCounter(event){
+      console.log(event)
+      console.log(this)
+      this.counter++
+
+      this.user.name += '!'
+    },
+    decCounter(){
+      this.counter--
+    }
+  },
+
   components: {
-    HelloWorld
+    Test
   }
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,33 @@
+<template>
+<!--<h1>test component {{text}}</h1>-->
+<!--  <button v-on:click="text">log</button>-->
+  <h3 v-for="item of items" :key="item.id">
+    {{item.text}} <button @click ="removeItem(item.id)">remove</button>
+  </h3>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  props: {
+    items:{
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    logger(){
+      console.log(this)
+    },
+
+    removeItem(id){
+      this.$emit('removeItem', id)
+    },
+  },
+  emits:['removeItem']
+}
+</script>
+
+<style scoped>
+
+</style>
